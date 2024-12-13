486407f60d62e7b38b4e46cad58fc8764867f96e
Velikoborets <gtahzby1@gmail.com>
2024-12-13 20:09:48 +0300

    init user module and create migrations and model for him

---
+++ modules/user/models/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $username
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%users}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username'], 'required'],
+            [['username'], 'string', 'max' => 255],
+            [['username'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+        ];
+    }
+}
