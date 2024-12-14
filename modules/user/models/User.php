e7836a78a022e136a60e7a01f604a9ab32043446
Velikoborets <gtahzby1@gmail.com>
2024-12-14 07:24:02 +0300

    create IdentityInterface for User model

--- modules/user/models/User.php
+++ modules/user/models/User.php
@@ -42,4 +42,46 @@ class User extends \yii\db\ActiveRecord
             'username' => 'Username',
         ];
     }
+
+    /**
+     * Finds user object by the given ID.
+     *
+     * @param int|string $id
+     * @return User|null object by id
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    /**
+     * Returns the unique ID of the user.
+     *
+     * @return int|string еhe unique identifier of the user.
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Given auth key
+     *
+     * @return string a key that is used to check validity
+     */
+    public function getAuthKey()
+    {
+        return '';
+    }
+
+    /**
+     * Validates the given authentication key.
+     *
+     * @param  string $authKey for validation
+     * @return bool (valid auth key or not)
+     */
+    public function validateAuthKey($authKey)
+    {
+        return true;
+    }
 }
